9113ab479b98a3a5ae7259fb816d568fda84e067
Megan H <megannh6349@gmail.com>
2020-07-11 17:15:25 -0400

    final draft

---
+++ assets/js/questions.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+        title: 'Which of these is not a way to set a variable?',
+        choices: ['var', 'let', 'const', 'make'],
+        answer: 'make'
+    },
+    {
+        title: 'Which of these affects an HTML table?',
+        choices: ['<tr>', '<thead>', '<tableEnd', '<td>'],
+        answer: '<tableEnd>'
+    },
+    {
+        title: 'What is the most current version of HTML?',
+        choices: ['HTML1', 'HTML5', 'HTML9', 'HTML12'],
+        answer: 'HTML5'
+    },
+    {
+        title: 'How hard did Megan and Carlos work on this?',
+        choices: ['Not hard at all', 'They just copied the whole thing', 'Barely tried', 'Bro we worked so hard on this you have no idea'],
+        answer: 'Bro we worked so hard on this you have no idea'
+    },
+    {
+        title: 'What is the correct way to use an ID in HTML/CSS?',
+        choices: ['#id', '$id', '.id', '?id'],
+        answer: '#id'
+    },
+    {
+        title: 'What is the correct way to use a class in HTML/CSS?',
+        choices: ['#class', '$class', '.class', '?class'],
+        answer: '.class'
+    },
+    {
+        title: 'Which of these would NOT set a color in HTML/CSS?',
+        choices: ['#000000', 'black', 'rgb(255, 99, 71)', '@2'],
+        answer: '@2'
+    },
+    {
+        title: 'What does HTML stand for?',
+        choices: ['Hypertext Markup Language', 'Hydrotext Manipulation Language', 'Home Text Manuscript Learning', 'Holy Trout, My Lentils!'],
+        answer: 'Hypertext Markup Language'
+    }, {
+        title: 'Which tag has the largest heading?',
+        choices: ['h1', 'h2', 'mega-h', 'hINFINITY'],
+        answer: 'h1'
+    },
+    {
+        title: 'Which HTML element do we store JavaScript in?',
+        choices: ['<script>', '<js>', '<javascript>', '<scripts>'],
+        answer: '<script>'
+    },
+];
+
+
+
+
+
